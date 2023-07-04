77528be2dd0ccd8d8f8c09cd9dd37ef8b2fd4d97
Rudrik01 <patelrudrik2004@gmail.com>
2023-07-04 22:32:26 +0530

    added gfg problem solution

---
+++ GFG/Bit_Difference.cpp
@@ -0,0 +1,31 @@
+/**
+██████╗░██╗░░░██╗██████╗░██████╗░██╗██╗░░██╗  ██████╗░░█████╗░████████╗███████╗██╗░░░░░
+██╔══██╗██║░░░██║██╔══██╗██╔══██╗██║██║░██╔╝  ██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║░░░░░
+██████╔╝██║░░░██║██║░░██║██████╔╝██║█████═╝░  ██████╔╝███████║░░░██║░░░█████╗░░██║░░░░░
+██╔══██╗██║░░░██║██║░░██║██╔══██╗██║██╔═██╗░  ██╔═══╝░██╔══██║░░░██║░░░██╔══╝░░██║░░░░░
+██║░░██║╚██████╔╝██████╔╝██║░░██║██║██║░╚██╗  ██║░░░░░██║░░██║░░░██║░░░███████╗███████╗
+╚═╝░░╚═╝░╚═════╝░╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚═╝  ╚═╝░░░░░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚══════╝
+Author:Rudrik Patel
+Topic:Bit Difference
+link: https://practice.geeksforgeeks.org/problems/bit-difference-1587115620/1
+Time Complexity: O(Set bit count)
+Auxiliary Space: O(1)
+*/
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        
+        // Your logic here
+        int temp=a^b;
+        int res=0;
+        while(temp>0){
+            temp=temp&(temp-1);
+            res++;
+        }
+        return res;
+        
+    }
+};
+
---
+++ GFG/Rightmost_different_bit.cpp
@@ -0,0 +1,30 @@
+/**
+██████╗░██╗░░░██╗██████╗░██████╗░██╗██╗░░██╗  ██████╗░░█████╗░████████╗███████╗██╗░░░░░
+██╔══██╗██║░░░██║██╔══██╗██╔══██╗██║██║░██╔╝  ██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║░░░░░
+██████╔╝██║░░░██║██║░░██║██████╔╝██║█████═╝░  ██████╔╝███████║░░░██║░░░█████╗░░██║░░░░░
+██╔══██╗██║░░░██║██║░░██║██╔══██╗██║██╔═██╗░  ██╔═══╝░██╔══██║░░░██║░░░██╔══╝░░██║░░░░░
+██║░░██║╚██████╔╝██████╔╝██║░░██║██║██║░╚██╗  ██║░░░░░██║░░██║░░░██║░░░███████╗███████╗
+╚═╝░░╚═╝░╚═════╝░╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚═╝  ╚═╝░░░░░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚══════╝
+Author:Rudrik Patel
+Topic: Rightmost different bit
+link: https://practice.geeksforgeeks.org/problems/rightmost-different-bit-1587115621/1
+Time Complexity: O(1)
+Auxiliary Space: O(1)
+*/
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        // Your code here
+        int temp = m^n;
+        for(int i = 0;i<31;i++){
+            if((temp&(1<<i))!=0){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
